0ad6f351d122e4cefcee9e22822af4852b4ef3aa
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-06 10:42:53 +0300

    Add the materialize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
+import Home from './components/Home.component';
 import './App.css';
 
 class App extends Component{
-  
+  render(){
+    return (
+        <Home></Home>
+    ); 
+  }
 }
 
 export default App;
---
+++ src/components/Home.component.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+
+    render(){
+        return (
+            <div className="">
+                {/* Side Div*/}
+                    <span className="blue-text">Blue</span>
+                {/*  Main Div */}
+                    <span className="red-text">Red</span>
+            </div>  
+        )
+    }
+}
+
+export default Home;
