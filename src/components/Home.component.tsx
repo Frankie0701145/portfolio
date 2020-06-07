7121969580710f2bd9ac48089560422df9843456
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-07 19:57:19 +0300

    Finish the sideNav bar.

--- src/components/Home.component.tsx
+++ src/components/Home.component.tsx
@@ -7,55 +7,59 @@ class Home extends Component {
 
     render(){
         return (
-            <div className="row" id="homeWrapper">
-                                                 {/* Side Div*/}
-                    <div className="col s3 red lighten-4" id="sideContent">
-                        
-                                            {/* Profile*/}
+            <div id="homeWrapper"  className="row">
 
-                         <div className="col s12">
+                {/* Side Nav */}
+                <div className="col s12 m12 l2 red darken-3" id="sideNav">
 
-                                      {/* Profile picture  */}
-                                <div className="col s12 center-align info">
-                                    <img src="images/profile.jpg" alt="" className="circle responsive-img" id="profilePic"/>
-                                </div>
+                    {/* Profile */}
+                    <div className="section center-align">
+                        <img src="images/profile.jpg" id="profilePic"  className="circle responsive-img"></img>
+                        <h5 id="name">Francis Njuguna</h5>
+                        <blockquote className="left-align">
+                            Hi, my name is Francis Njuguna and I'm a full stack Software Developer. Welcome to my website!
+                        </blockquote>
+                    </div>
+                    
+                    <div className="section">
+                        <div className="divider black"></div>
+                        <div className="row">
+                             <a href="#" className="col s12"> 
+                                <div className="col s12 navLinks">Portfolio</div>
+                            </a>
+                        </div>
 
-                                            {/* Name */}
-                                <div className="col s12 center-align info">
-                                        Francis Njuguna
-                                </div>
-
-                                    {/* Description */}
-                                <div className="col s12 info">
-                                    Hi,I my name is Francis Njuguna and I am Full Stack Developer. Welcome to my personal website
-                                </div>
-
-                                    {/* Social Media icons */}
-                                <div className="col s12">
-                                    <div className="row">
-                                        <a className="waves-effect waves-light btn social google">
-                                            <i className="fa fa-google"></i> Sign in with Google
-                                        </a>
-                                    </div>
-                                </div>
-
-                                    {/* Nav Links */}
-                                <div className="col s12">
-                                      <a className="col s12" href="#">About Me</a>
-                                      <a className="col s12" href="#">Portfolio</a>
-                                      <a className="col s12" href="#">Skills</a>
-                                </div>
-         
-                         </div>
-
+                        <div className="row active">
+                            <a href="#" className="col s12">
+                                <div className="col s12 navLinks">Skills </div>
+                            </a>
+                        </div>
+                        <div className="row">
+                            <a href="#" className="col s12">
+                                <div className="col s12 navLinks "> About Me</div>
+                            </a>                 
+                        </div>
                     </div>
 
-                                       {/*  Main Div */}
-                    <div className="col s7 border">
-                        <span className="red-text">Red</span>
-                    </div> 
+                    
+                    <div className="section">
+                       <div className="divider black"></div>
+                       <div className="row">
+                            <div className="col ">
+                                <i className="material-icons icons col center-align">alarm</i>
+                                <i className="material-icons icons col center-align">attach_file</i>
+                                <i className="material-icons icons col center-align">backup</i>
+                            </div>
+                       </div>
+                    </div>
 
-            </div>  
+                </div>
+
+                {/* Main Nav */}
+                <div className="col s12 m12 l10" id="mainNav">
+
+                </div>
+            </div>
         )
     }
 }
