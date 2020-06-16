3ab70cde4b2331cfe76e8a99655b3175c2ae9ef1
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-16 12:00:07 +0300

    Updates

---
+++ precache-manifest.0b0f2e8c0de7209b6e31ee1a6ebc497f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a451a1b00c19f03ff03775b1661ccc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "98f6d1a9777989b859f4",
+    "url": "/Portfolio/static/css/2.a0af25ca.chunk.css"
+  },
+  {
+    "revision": "9dde9d93c9e452ef261d",
+    "url": "/Portfolio/static/css/main.8e4446c6.chunk.css"
+  },
+  {
+    "revision": "98f6d1a9777989b859f4",
+    "url": "/Portfolio/static/js/2.7d4ab3be.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/Portfolio/static/js/2.7d4ab3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dde9d93c9e452ef261d",
+    "url": "/Portfolio/static/js/main.57d61af5.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  }
+]);
