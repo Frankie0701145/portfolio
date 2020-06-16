f9b252b74b90568d126d21563289bfe535ebcf7a
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-16 10:44:26 +0300

    Adds the skills view.

--- src/components/Skills.component.tsx
+++ src/components/Skills.component.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import "./Skills.component.css";
 
 class Skills extends Component{
     render(){
@@ -15,7 +15,105 @@ class Skills extends Component{
                 </div>
 
                 <div className="container">
-                    
+                    <div className="row">
+                            <div className="col s12">
+                                <h6 className="title">Skills Overview</h6>
+                            </div>
+                            <div className="col s12">
+                                    <p>
+                                        I am a full-stack developer specialized in frontend and backend development for complex
+                                        scalable web apps.
+                                    </p>
+                            </div>
+                    </div>
+
+                    <div className="row skillsWrapper">
+
+                            {/* Frontend */}
+                            <div className="col s12 m6">
+                                <div className="card small">
+                                    {/* A round picture */}
+                                    <i className="material-icons imgRound align-center">web</i>
+                                    <div className="card-content blue">
+                                        <h6 className="card-title center-align">Frontend</h6>
+                                        <ul>
+                                            <li>React/Redux</li>
+                                            <li>Angular/NgRx</li>
+                                            <li>HTML/CSS/SASS/LESS</li>
+                                            <li>Material/Bootstrap/Ant Design</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Backend */}
+                            <div className="col s12 m6">
+                                <div className="card small">
+                                    {/* A round picture */}
+                                    <i className="material-icons imgRound align-center">web</i>
+                                     <div className="card-content blue">
+                                        <h6 className="card-title center-align">Backend</h6>
+                                        <ul>
+                                            <li>Node</li>
+                                            <li>Ruby/Rails</li>
+                                            <li>MongoDb/PostgreSQL</li>
+                                            <li>Redis</li>
+                                        </ul>
+                                     </div>
+                                </div>
+                            </div>
+
+                            {/* Devops */}
+                            <div className="col s12 m6">
+                                <div className="card small">
+                                    {/* A round picture */}
+                                    <i className="material-icons imgRound align-center">web</i>
+                                    <div className="card-content blue">
+                                        <h6 className="card-title center-align">Devops</h6>
+                                        <ul>
+                                            <li>Continuous Integration</li>
+                                            <li>Continuous Development</li>
+                                            <li>Nginix</li>
+                                            <li>Docker</li>
+                                        </ul>
+                                    </div>
+                                </div>      
+                            </div>
+
+                            {/* Others */}
+                            <div className="col s12 m6">
+                                <div className="card small">
+                                    {/* A round picture */}
+                                    <i className="material-icons imgRound align-center">web</i>
+                                    <div className="card-content blue">
+                                        <h6 className="card-title center-align">Others</h6>
+                                        <ul>
+                                            <li>UI/UX</li>
+                                            <li>React Native</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                        {/* Languages */}
+                            <div className="col s12 m7 offset-m3" id="last-child">
+                                <div className="card small">
+                                    {/* A round picture */}
+                                    <i className="material-icons imgRound align-center">web</i>
+                                    <div className="card-content blue">
+                                        <h6 className="card-title center-align">Programming</h6>
+                                        <ul>
+                                            <li>JavaScript/NodeJs/TypeScript</li>
+                                            <li>Ruby</li>
+                                            <li>Python</li>
+                                            <li>Java</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+
                 </div>
 
             </div>
